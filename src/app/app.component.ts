d237dfd41354dbae7b102101f49897b27ecb6450
Alex <agamyan@outlook.com>
2021-05-15 21:20:25 +0400

    ...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: [ 'app.component.scss' ]
+  styleUrls: ['app.component.scss']
 })
 
 export class AppComponent {
