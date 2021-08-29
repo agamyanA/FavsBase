33d3c6d48acc221f903409d6e0945f4aa2f64e02
Alex <agamyan@outlook.com>
2021-08-29 15:52:07 +0400

    removed unnecessary imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,31 +6,25 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 
-import { HomeModule } from './pages/home/home.module';
-import { DashboardModule } from './pages/dashboard/dashboard.module';
-
 import { TuiRootModule, TuiThemeNightModule } from '@taiga-ui/core';
 import { TuiToggleModule } from '@taiga-ui/kit';
 
 import { AngularFireModule } from '@angular/fire';
 
 import { AppComponent } from './app.component';
-import { PageNotFoundComponent } from './pages/404/page-not-found.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    HomeModule,
-    DashboardModule,
     TuiRootModule,
     TuiThemeNightModule,
     TuiToggleModule,
     ReactiveFormsModule,
     AngularFireModule.initializeApp(environment.firebaseConfig)
   ],
-  declarations: [AppComponent, PageNotFoundComponent],
+  declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
 
